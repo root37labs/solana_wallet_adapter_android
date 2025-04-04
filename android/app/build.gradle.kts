d2de2655d30fcc1eddd790e718488e529b19d64c
imac <imac@imacui-iMac.local>
2025-04-04 13:43:56 +0900

    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.merigo.solana_wallet_adapter_android"
+        applicationId = "com.root37.solana_wallet_adapter_android"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
