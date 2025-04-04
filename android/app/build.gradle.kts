9874fe9ca65574ad294cd5cefd6579a7ab2c0f40
imac <imac@imacui-iMac.local>
2025-04-04 13:54:59 +0900

    원복

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,17 +11,17 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = '17'
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.root37.solana_wallet_adapter_android"
+        applicationId = "com.merigo.solana_wallet_adapter_android"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
